c195985827ef5864eeaaf45ff6eb91333d4f2222
Abdullah al rafi <abdullah116632@gmail.com>
2025-03-27 14:16:04 +0600

    navbar, footer & hero section

---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+    name: "ABDULLAH AL RAFI",
+    profile: '/profile.jpg',
+    designation: "Full-stack Developer",
+    description: "My name is Abdullah Al Rafi. I am a professional and enthusiastic programmer in my daily life. I am a Computer Science and Engineering (CSE) student with a self-learning attitude and a passion for problem-solving. I love to explore new technologies and build scalable web applications. My core skill is based on JavaScript, and I enjoy working with the MERN stack to develop full-stack projects. I am currently looking for freelancing opportunities where I can apply my skills and gain real-world experience. I am available for any job opportunity that suits my expertise and interests.",
+    email: 'abdullah116632@gmail.com',
+    phone: '+8801768899941',
+    address: 'Kazipur, Sirajganj, Bangladesh ',
+    github: 'https://github.com/abdullah116632',
+    facebook: 'https://www.facebook.com/profile.php?id=100084115466829',
+    linkedIn: 'https://www.linkedin.com/in/abdullah116632/',
+    twitter: 'https://x.com/Abdullah116632',
+    stackOverflow: 'https://stackoverflow.com/users/22153456/abdullah',
+    leetcode: "https://leetcode.com/u/abdullah2040/",
+    devUsername: "abdullah116632",
+    resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+  }
